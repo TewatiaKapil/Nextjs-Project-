73bee167269040bb49142e8e0e509ccea7f6c211
TewatiaKapil <115689668+TewatiaKapil@users.noreply.github.com>
2024-04-17 16:01:39 +0530

    Add files via upload

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Dqk5sk8POM+hsNRCv92NVdqKMSbeOdGoGtGgesRNjko=\"\n}"
---
+++ .next/server/vendor-chunks/@syncfusion.js
@@ -0,0 +1,94 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@syncfusion";
+exports.ids = ["vendor-chunks/@syncfusion"];
+exports.modules = {
+
+/***/ "./node_modules/@syncfusion/ej2-base/styles/material.css":
+/*!***************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-base/styles/material.css ***!
+  \***************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-buttons/styles/material.css":
+/*!******************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-buttons/styles/material.css ***!
+  \******************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-calendars/styles/material.css":
+/*!********************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-calendars/styles/material.css ***!
+  \********************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-dropdowns/styles/material.css":
+/*!********************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-dropdowns/styles/material.css ***!
+  \********************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-inputs/styles/material.css":
+/*!*****************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-inputs/styles/material.css ***!
+  \*****************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-navigations/styles/material.css":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-navigations/styles/material.css ***!
+  \**********************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-popups/styles/material.css":
+/*!*****************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-popups/styles/material.css ***!
+  \*****************************************************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "./node_modules/@syncfusion/ej2-react-schedule/styles/material.css":
+/*!*************************************************************************!*\
+  !*** ./node_modules/@syncfusion/ej2-react-schedule/styles/material.css ***!
+  \*************************************************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
